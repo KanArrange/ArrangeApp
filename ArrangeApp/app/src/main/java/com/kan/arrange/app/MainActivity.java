61d67da411723a0c4e8252427b11b36f00e342a1
kanl212@163.com <kanl212@163.com>
2016-11-03 20:08:33 +0800

    修改

--- ArrangeApp/app/src/main/java/com/kan/arrange/app/MainActivity.java
+++ ArrangeApp/app/src/main/java/com/kan/arrange/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.kan.arrange.R;
+import com.kan.arrange.notification.NotificationHandler;
 import com.kan.arrange.widget.WhorlView;
 
 public class MainActivity extends Activity {
@@ -14,5 +15,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NotificationHandler.getInstance(this).createProgressNotification(this);
+        NotificationHandler.getInstance(this).createButtonNotification(this);
+        NotificationHandler.getInstance(this).createExpandableNotification(this);
+        NotificationHandler.getInstance(this).createSimpleNotification(this);
     }
 }
