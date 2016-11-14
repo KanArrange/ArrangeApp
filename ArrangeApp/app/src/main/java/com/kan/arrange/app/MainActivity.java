f8903e25dba1e5131e038a7fc0db904188cafb46
kanl212@163.com <kanl212@163.com>
2016-11-14 19:31:16 +0800

    引入EventBus进行测试

--- ArrangeApp/app/src/main/java/com/kan/arrange/app/MainActivity.java
+++ ArrangeApp/app/src/main/java/com/kan/arrange/app/MainActivity.java
@@ -4,20 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.kan.arrange.R;
-import com.kan.arrange.notification.NotificationHandler;
-import com.kan.arrange.widget.WhorlView;
 
 public class MainActivity extends Activity {
 
-    private WhorlView mWhorlView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NotificationHandler.getInstance(this).createProgressNotification(this);
-        NotificationHandler.getInstance(this).createButtonNotification(this);
-        NotificationHandler.getInstance(this).createExpandableNotification(this);
-        NotificationHandler.getInstance(this).createSimpleNotification(this);
     }
 }
